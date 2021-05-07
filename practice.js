a9c8b41eaa3faca49d3a28eedcb273c0b367d439
hyun6 <psymhs@gmail.com>
2021-05-07 21:18:06 +0900

    impl find(f, iter) function

--- practice.js
+++ practice.js
@@ -13,3 +13,22 @@ const queryStr = pipe(
 )
 const queryStrRes = queryStr({key1: 'val1', key2: 'val2', key3: 3});
 log(queryStrRes);
+
+// find first filtered item
+const find = (f, iter) => go(
+    iter,
+    L.filter(a => (log(a), f(a))), // L.filter() evaluate until find item but filter() evaluate all iter
+    a => (log(a), a), // log for see iterated item
+    take(1),
+    ([u]) => u // array to obj
+)
+
+const users = [
+    {age: 18},
+    {age: 21},
+    {age: 23},
+    {age: 21},
+    {age: 37}
+]
+
+log(find(u => u.age === 23, users))
