341254d41c6f2b7acc1f88dec4cfb9cc2c4e76c8
hyun6 <psymhs@gmail.com>
2021-05-08 22:17:30 +0900

    practice real world example

--- practice.js
+++ practice.js
@@ -55,7 +55,7 @@ log(
 );
 
 // flatMap with iterable
-L.flatMap = pipe(L.map, L.flatten);
+L.flatMap = curry(pipe(L.map, L.flatten));
 
 // make flatMap
 const flatMap = curry(pipe(L.map, flatten));
@@ -101,4 +101,88 @@ log(
             map((a) => a + 1, [1, 2, 3])
         )
     )
+);
+
+// 2D array with lazy flatten
+log("2D array");
+const arr = [
+    [1, 2, 3],
+    [4, 5],
+    [6, 7, 8],
+    [9, 10],
+];
+
+go(
+    arr,
+    flatten,
+    map((a) => (log(a), a)), // iterate all items
+    filter((a) => a % 2),
+    take(3),
+    log
+);
+
+go(
+    arr,
+    L.flatten,
+    L.map((a) => (log(a), a)), // iterate only necessary items
+    L.filter((a) => a % 2),
+    take(3),
+    log
+);
+
+// real world example
+log('real world example');
+
+var realUsers = [
+    {
+        name: "a",
+        age: 21,
+        family: [
+            { name: "a1", age: 53 },
+            { name: "a2", age: 47 },
+            { name: "a3", age: 16 },
+            { name: "a4", age: 15 },
+        ],
+    },
+    {
+        name: "b",
+        age: 24,
+        family: [
+            { name: "b1", age: 58 },
+            { name: "b2", age: 51 },
+            { name: "b3", age: 19 },
+            { name: "b4", age: 22 },
+        ],
+    },
+    {
+        name: "c",
+        age: 31,
+        family: [
+            { name: "c1", age: 64 },
+            { name: "c2", age: 62 },
+        ],
+    },
+    {
+        name: "d",
+        age: 20,
+        family: [
+            { name: "d1", age: 42 },
+            { name: "d2", age: 42 },
+            { name: "d3", age: 11 },
+            { name: "d4", age: 7 },
+        ],
+    },
+];
+
+// iterable programming pattern: (data), [map, filter, flatten ...], [take, reduce ...], ...
+go(
+    realUsers,
+    // L.map(u => u.family),
+    // L.flatten,
+    L.flatMap((u) => u.family), // map, flatten === flatMap
+    L.filter((u) => u.age > 20),
+    L.map((u) => u.age),
+    take(4),
+    reduce((acc, age) => acc + age),
+    log
 );
